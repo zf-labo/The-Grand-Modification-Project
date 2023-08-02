673bb897cbb673a57be3c939b5a0880790e06fc2
LeZach <48424078+zf-development@users.noreply.github.com>
2023-08-01 21:58:27 -0400

    Initial Commit

---
+++ gmp-vehicle-assets/fxmanifest.lua
@@ -0,0 +1,7 @@
+fx_version 'cerulean'
+game 'gta5'
+lua54 'yes'
+
+author 'ZF Labo'
+description 'Lore Friendly Vehicle Assets'
+version '1.0.0'
---
+++ gmp-vehicle-sounds/fxmanifest.lua
@@ -0,0 +1,28 @@
+fx_version 'cerulean'
+game 'gta5'
+lua54 'yes'
+
+author 'ZF Labo'
+description 'Lore Friendly Vehicle Sounds'
+version '1.0.0'
+
+data_file 'AUDIO_GAMEDATA' 'vehicle_sfx/**/*_game.dat'
+data_file 'AUDIO_SOUNDDATA' 'vehicle_sfx/**/*_sounds.dat'
+data_file 'AUDIO_WAVEPACK' 'vehicle_sfx/**/*.awc'
+
+files {
+    -- dat10
+	'vehicle_sfx/**/*.dat10',
+	'vehicle_sfx/**/*.dat10.nametable',
+	'vehicle_sfx/**/*.dat10.rel',
+    -- dat151
+	'vehicle_sfx/**/*.dat151',
+	'vehicle_sfx/**/*.dat151.nametable',
+	'vehicle_sfx/**/*.dat151.rel',
+    -- dat54
+	'vehicle_sfx/**/*.dat54',
+	'vehicle_sfx/**/*.dat54.nametable',
+	'vehicle_sfx/**/*.dat54.rel',
+    -- awc
+	'vehicle_sfx/**/*.awc',
+}
